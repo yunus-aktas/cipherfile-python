ef76ae5f9efe2261a3e7039ca12d2233d8a3ebd2
Yunus Aktaş <aktasyunus597@gmail.com>
2026-02-21 02:36:03 +0300

    main.py menu yapisi eklendi

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-# Bu dosya programin ana giris noktasi olacak
+from encrypt import encrypt_file
+from decrypt import decrypt_file# Bu dosya programin ana giris noktasi olacak
 # Menu ve kullanici secimleri buradan yonetilecek
+print("1 - Dosya Şifrele")
+print("2 - Dosya Çöz")
+print("3 - Çıkış")
+
+secim = input("Seçiminizi girin: ")
+
+if secim == "1":
+    print("Şifreleme seçildi")
+elif secim == "2":
+    print("Çözme seçildi")
+elif secim == "3":
+    print("Çıkış yapılıyor")
+else:
+    print("Geçersiz seçim")
