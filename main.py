96b5b4e97260d11461dbe64041a3fbc00b07cbe1
Yunus Aktaş <aktasyunus597@gmail.com>
2026-02-21 17:48:19 +0300

    main menu while dongusu eklendi

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from encrypt import encrypt_file
 from decrypt import decrypt_file
 
-print("1 - Dosya Şifrele")
-print("2 - Dosya Çöz")
-print("3 - Çıkış")
+while True:
+    print("\n--- DOSYA ŞİFRELEME PROGRAMI ---")
+    print("1 - Dosya Şifrele")
+    print("2 - Dosya Çöz")
+    print("3 - Çıkış")
 
-secim = input("Seçiminizi girin: ")
+    secim = input("Seçiminizi girin: ")
 
-if secim == "1":
-    encrypt_file()
-elif secim == "2":
-    decrypt_file()
-elif secim == "3":
-    print("Çıkış yapılıyor")
-else:
-    print("Geçersiz seçim")
+    if secim == "1":
+        encrypt_file()
+
+    elif secim == "2":
+        decrypt_file()
+
+    elif secim == "3":
+        print("Çıkış yapılıyor... 👋")
+        break
+
+    else:
+        print("❌ Geçersiz seçim! Lütfen 1, 2 veya 3 girin.")
